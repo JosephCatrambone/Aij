13ac5753b7e997531034b0ed4f22da4b0dd34365
Joseph Catrambone <jo.jcat@gmail.com>
2015-11-01 17:14:06 -0800

    Fix convolution test.  Conv is COLUMN MAJOR.

--- src/test/java/ConvolutionTest.java
+++ src/test/java/ConvolutionTest.java
@@ -3,8 +3,6 @@ import com.josephcatrambone.aij.networks.ConvolutionalNetwork;
 import com.josephcatrambone.aij.networks.FunctionNetwork;
 import org.junit.Test;
 
-import java.util.function.Consumer;
-
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
@@ -19,55 +17,24 @@ public class ConvolutionTest {
 		// 1 2 3 4 5
 		// 6 7 8 9 0
 		// 1 2 3 4 5
-		Matrix m = new Matrix(new double[][] {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}});
-		FunctionNetwork oneToOne = new FunctionNetwork(9, 0); // 3x3
-		ConvolutionalNetwork cn = new ConvolutionalNetwork(oneToOne, 5, 5, 3, 3, 3, 3, 1, 1, ConvolutionalNetwork.EdgeBehavior.ZEROS);
+		Matrix m = new Matrix(new double[][] {{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5}});
+		FunctionNetwork oneToOne = new FunctionNetwork(9, 1); // 3x3 -> 1x1
+		ConvolutionalNetwork cn = new ConvolutionalNetwork(oneToOne, 5, 5, 3, 3, 1, 1, 1, 1, ConvolutionalNetwork.EdgeBehavior.ZEROS);
 
-		oneToOne.predictionMonitor = new Consumer<Matrix>() {
-			int i=0;
-			@Override
-			public void accept(Matrix intermediate) {
-				switch(i) {
-					case 0:
-						// 0 0 0
-						// 0 1 2
-						// 0 6 7
-						assertArrayEquals(intermediate.getRowArray(0), new double[] {0, 0, 0, 0, 1, 2, 0, 6, 7}, 1e-5);
-						break;
-					case 1:
-						// 0 0 0
-						// 1 2 3
-						// 6 7 8
-						assertArrayEquals(intermediate.getRowArray(0), new double[] {0, 0, 0, 1, 2, 3, 6, 7, 8}, 1e-5);
-						break;
-					case 2:
-						// 0 0 0
-						// 2 3 4
-						// 7 8 9
-						assertArrayEquals(intermediate.getRowArray(0), new double[] {0, 0, 0, 2, 3, 4, 7, 8, 9}, 1e-5);
-						break;
-					case 3:
-						// 0 0 0
-						// 3 4 5
-						// 8 9 10
-						assertArrayEquals(intermediate.getRowArray(0), new double[] {0, 0, 0, 3, 4, 5, 8, 9, 10}, 1e-5);
-						break;
-					case 4:
-						// 0 0 0
-						// 4 5 0
-						// 9 10 0
-						assertArrayEquals(intermediate.getRowArray(0), new double[] {0, 0, 0, 4, 5, 0, 9, 10, 0}, 1e-5);
-						break;
-					case 5:
-						// 0 1 2
-						// 0 6 7
-						// 0 11 12
-						assertArrayEquals(intermediate.getRowArray(0), new double[] {0, 1, 2, 0, 6, 7, 0, 11, 12}, 1e-5);
-						break;
-				}
-				i++;
-			}
-		};
+		oneToOne.predictionFunction = (Matrix k) -> new Matrix(1, 1, k.sum());
+
+		assertEquals(oneToOne.predict(new Matrix(new double[][]{{1, 2, 3}})).get(0, 0), 6.0, 1.0e-7);
+
+		Matrix result = cn.predict(m);
+
+		System.out.println(result);
+		assertArrayEquals(result.getRowArray(0), new double[]{
+				16, 27, 33, 29, 18,
+				19, 33, 42, 41, 27,
+				29, 48, 57, 46, 27,
+				19, 33, 42, 41, 27,
+				16, 27, 33, 29, 18
+		}, 1e-5);
 	}
 
 }
