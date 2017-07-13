c05ab64e4027cf56de29a8a8a8d4400ccc0bc189
Joseph Catrambone <jo.jcat@gmail.com>
2017-07-12 22:34:02 -0700

    Move models into a separate module.  Need to refactor model still.  Add LSTM.

---
+++ src/main/java/com/josephcatrambone/aij/LSTM.java
@@ -0,0 +1,260 @@
+package com.josephcatrambone.aij;
+
+import com.josephcatrambone.aij.nodes.*;
+import com.josephcatrambone.aij.optimizers.Optimizer;
+
+import java.util.Random;
+
+/**
+ * Created by jcatrambone on 7/12/17.
+ */
+public class LSTM {
+	private Random random = new Random();
+
+	int inputSize = -1;
+	int hiddenSize = -1;
+	VariableNode weight_ig; // Wi*x_t  // X is our input.  i means input gate.
+	VariableNode weight_hg; // Ui*h_t-1
+	VariableNode weight_if; // W_f*x_t
+	VariableNode weight_hf; // U_f*h_t-1
+	VariableNode weight_ic; // W_c*x_t
+	VariableNode weight_hc; // U_c*h_t-1
+	VariableNode weight_io;
+	VariableNode weight_ho;
+	VariableNode bias_g;
+	VariableNode bias_f;
+	VariableNode bias_c;
+	VariableNode bias_o;
+
+	Optimizer optimizer = null;
+	Graph trainingGraph = null;
+	LSTMStep[] steps = null;
+	Graph runGraph = null;
+
+	public LSTM(int inputSize, int hiddenSize) {
+		this.inputSize = inputSize;
+		this.hiddenSize = hiddenSize;
+
+		final double WEIGHT_SCALE = 0.1;
+
+		weight_ig = new VariableNode(new Matrix(inputSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		weight_if = new VariableNode(new Matrix(inputSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		weight_ic = new VariableNode(new Matrix(inputSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		weight_io = new VariableNode(new Matrix(inputSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		weight_hg = new VariableNode(new Matrix(hiddenSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		weight_hf = new VariableNode(new Matrix(hiddenSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		weight_hc = new VariableNode(new Matrix(hiddenSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		weight_ho = new VariableNode(new Matrix(hiddenSize, hiddenSize, (i,j)->random.nextGaussian()*WEIGHT_SCALE));
+		bias_g = new VariableNode(1, hiddenSize);
+		bias_f = new VariableNode(1, hiddenSize);
+		bias_c = new VariableNode(1, hiddenSize);
+		bias_o = new VariableNode(1, hiddenSize);
+	}
+
+	/*** unrollAndTrain
+	 * Makes an unrolled LSTM which will calculate a bunch of inputs at the same time.
+	 * We assume the input matrices are in the following format.
+	 * inputs[0] = a bunch of examples = [number of examples in this batch, the size of one step].
+	 * outputs[0] = the expected outputs.
+	 * For example, if we had a sequence of length 5, "my cat is an asshole", our inputs[] would be length 4 & outputs length 4.
+	 * inputs[0] would be 1 row x <number of words> columns
+	 * If we had a bunch of sentences to compute in parallel,
+	 * inputs[0] would be k rows x <number of keywords> columnts.
+	 * "my dog is very sweet" and "What a terrible, stupid encoding scheme" would yield an input matrix array of size
+	 * [5][2,dictionary length]
+	 * input[0] = ["my" ]  output[0] = ["dog"]  input[1] = ["dog"]  output[1] = ["is"]
+	 *            ["what"]             ["a" ]   input[1] = ["a"]    output[1] = ["terrible"]
+	 * If our dict were [my dog is very sweet what a terrible stupid encoding scheme], then
+	 * input[0] = [1 0 0 0 0 0 0 0 0 0 0]
+	 *            [0 0 0 0 0 1 0 0 0 0 0]
+	 * @param inputs
+	 * @param outputs
+	 * @param stepsToUnwind
+	 * @return
+	 */
+	float unrollAndTrain(Matrix[] inputs, Matrix[] outputs, int stepsToUnwind) {
+		if(trainingGraph == null) {
+			steps = new LSTMStep[stepsToUnwind];
+			trainingGraph = new Graph();
+
+			// Start with some default input item.
+			LSTMStep startStep = new LSTMStep(this);
+			startStep.input = new InputNode(inputs[0].rows, inputSize);
+			startStep.hidden = new ConstantNode(inputs[0].rows, hiddenSize, 0.0);
+			startStep.memory = new ConstantNode(inputs[0].rows, hiddenSize, 0.0);
+
+			steps[0] = startStep;
+			for(int i=1; i < stepsToUnwind; i++) {
+				Node input = new InputNode(inputs[0].rows, inputSize);
+				steps[i] = steps[i-1].wireNextStep(input);
+			}
+
+			// Wire up a loss for all of them.
+
+			// Add all of them to the training graph.
+			for(LSTMStep step : steps) {
+				trainingGraph.addNode(step.hidden);
+				// TODO: Start here.
+			}
+		}
+
+
+	}
+
+	class LSTMStep {
+		LSTM parent;
+
+		// These aren't actually part of the network, but they're useful to track here.
+		Node input;
+		Node target = null;
+		Node loss = null;
+
+		// Taken from Siamese Recurrent Architectures by Muller & Thyagarajan (2016).
+		Node gate;
+		Node forget;
+		Node memory;
+		Node out;
+		Node hidden;
+
+		LSTMStep(LSTM parentLSTM) {
+			// i_c = sigmoid(Wixt + uiht-1 + bi)
+			this.parent = parentLSTM;
+		}
+
+		LSTMStep wireNextStep(Node input) {
+			LSTMStep next = new LSTMStep(this.parent);
+
+			next.input = input;
+			next.gate = new SigmoidNode(new AddNode(new AddNode(new MatrixMultiplyNode(input, parent.weight_ig), new MatrixMultiplyNode(this.hidden, parent.weight_hg)), parent.bias_g));
+			next.forget = new SigmoidNode(new AddNode(new AddNode(new MatrixMultiplyNode(input, parent.weight_if), new MatrixMultiplyNode(this.hidden, parent.weight_hf)), parent.bias_f));
+			Node memoryHat = new TanhNode(new AddNode(new AddNode(new MatrixMultiplyNode(input, parent.weight_ic), new MatrixMultiplyNode(this.hidden, parent.weight_hc)), parent.bias_c));
+			next.memory = new AddNode(new MultiplyNode(next.gate, memoryHat), new MultiplyNode(next.forget, this.memory));
+			next.out = new SigmoidNode(new AddNode(new AddNode(new MatrixMultiplyNode(input, parent.weight_io), new MatrixMultiplyNode(this.hidden, parent.weight_ho)), parent.bias_o));
+			next.hidden = new MultiplyNode(next.out, new TanhNode(next.memory));
+
+			return next;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+I feel useless.
+I've felt useless for a while.  At work, my sole purpose is to do machine learning stuff, but I'm not very good at it.
+I've not made anything in production that's being used.  The stuff I have made is being replaced.
+First it was LDA topics.  I couldn't calculate them and we had to bring in an outside team to do it.
+Next was doing the similarity DB.  My solution to store them in the database is being replaced by the data team.
+Next is the home front.  Someone without AI experience has an offer to work at OpenAI.  :(  Good for him, I mean,
+but darn for me.  I can't seem to make anything of value.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/RNNTest.java
+++ src/test/java/RNNTest.java
@@ -14,190 +14,13 @@ public class RNNTest {
 
 	public Random random = new Random();
 	public int inputSize = 127;
-	public int hiddenSize = 10;
-	public Matrix w_ih = new Matrix(inputSize, hiddenSize, (i,j) -> random.nextGaussian()*0.01);
-	public Matrix w_hh = new Matrix(hiddenSize, hiddenSize, (i,j) -> random.nextGaussian()*0.01);
-	public Matrix w_ho = new Matrix(hiddenSize, inputSize, (i,j) -> random.nextGaussian()*0.01);
-	public Matrix b_h = new Matrix(1, hiddenSize);
-	public Matrix b_o = new Matrix(1, inputSize);
-
-	// Training network, used only to train the model.
-	private int numTrainingStepsUnrolled = 25;
-	private Graph trainingGraph = null;
-	private Node[] trainingVariables;
-	private Node[] trainingInputNodes;
-	private Node[] trainingTargetNodes;
-	private Node trainingStartHiddenNode;
-	private Node trainingOutputHiddenNode;
-	private Node lossNode;
-
-	// Sampling section.
-	private Graph runGraph = null;
-	private Node sampleInputNode;
-	private Node sampleHiddenInputNode;
-	private Node sampleHiddenOutputNode;
-	private Node sampleOutputNode;
+	public int hiddenSize = 16;
 
 	private String echoStringWhileLessThanMinLength(String s, int minLength) {
 		do {
 			s = s + "~" + s; // At least one duplicate.
 		} while(s.length() < minLength);
 		return s;
-	}
-
-	/*** makeRNN
-	 * Given the previous hidden, constructs an RNN step.
-	 * @param g The Graph to which we should add these nodes.
-	 * @param hidden
-	 * @return Returns an array with the inputNode, nextHidden, outputNode, and (if enabled) the target and loss nodes.
-	 */
-	public Node[] makeRNN(Graph g, VariableNode weight_ih, VariableNode weight_hh, VariableNode weight_ho, VariableNode bias_h, VariableNode bias_o, Node hidden, boolean includeLossNode) {
-		Node[] result = null;
-		Node inputNode = new InputNode(1, inputSize);
-
-		// Do the actual wiring.
-		Node hiddenState = new TanhNode(new AddNode(
-			new TanhNode(new AddNode(new MatrixMultiplyNode(hidden, weight_hh), bias_h)),
-			new TanhNode(new MatrixMultiplyNode(inputNode, weight_ih))
-		));
-		//Node outputState = new SigmoidNode(new AddNode(new MatrixMultiplyNode(hiddenState, weight_ho), bias_o));
-		Node preactOutput = new AddNode(new MatrixMultiplyNode(hiddenState, weight_ho), bias_o);
-		Node outputNode = new SigmoidNode(preactOutput); // This may need to change, depending on the loss.
-
-		// If we want to train, we need to wire up the loss.
-		if(includeLossNode) {
-			result = new Node[5];
-
-			result[0] = inputNode;
-			result[1] = hiddenState;
-			result[2] = outputNode;
-			Node target = new InputNode(preactOutput.rows, inputSize); // Target node.
-			result[3] = target;
-			// ABS error
-			//result[4] = new AbsNode(new SubtractNode(target, preactOutput));
-
-			// Squared error.
-			//Node diff = new SubtractNode(result[3], outputState);
-			//result[4] = new PowerNode(diff, 2);
-
-			// KL Divergence = sum y_i log y_i/out_i
-			//result[4] = new MultiplyNode(target, new LogNode(new MultiplyNode(target, new InverseNode(result[2]))));
-
-			// XEnt Loss
-			// - sum{ y*log(pred) + (1-y)log(1-pred) }
-			result[4] = new NegateNode(new AddNode(
-				new MultiplyNode(target, new LogNode(new AddNode(new ConstantNode(1e-6, outputNode), outputNode))), // y*log(pred)
-				new MultiplyNode(
-					new SubtractNode(new ConstantNode(1.0, target), target), // 1-y
-					new LogNode(new AddNode(new ConstantNode(1e-6, outputNode), new SubtractNode(new ConstantNode(1.0, outputNode), outputNode))) // log(1-pred)
-				)
-			));
-
-			// Softmax loss.
-			// If our output is not normalized (or is a ReLU), dLoss/dOut = softmax(out) - [all zeros, except a 1 for the correct label]
-			// Note that we are _NOT_ activating the output layer.
-			// If we activate before softmax, bad things happen.  If we use the softmax node as a 'softmax loss', bad things happen, too.
-			// Instead, calculate the softmax of the practivated state by hand and back-propagate a nice loss.
-			//result[4] = new SoftmaxLossNode(preactOutput, target);
-
-			g.addNode(result[4]);
-		} else {
-			result = new Node[3];
-
-			result[0] = inputNode;
-			result[1] = hiddenState;
-			result[2] = outputNode;
-			g.addNode(result[2]);
-		}
-
-		return result;
-	}
-
-	/*** unrollAndTrain
-	 * Unroll i.length loops into a graph, learn the grads, sum them, and return them.
-	 * @param inputData
-	 * @param outputData
-	 * @return
-	 */
-	public Matrix[] unrollAndTrain(double[][] inputData, double[][] outputData, DoubleConsumer reportFunction) {
-
-		HashMap <Node, Matrix> inputMap = new HashMap<>();
-
-		if(trainingGraph == null) {
-			VariableNode weight_ih = new VariableNode(w_ih);
-			VariableNode weight_hh = new VariableNode(w_hh);
-			VariableNode weight_ho = new VariableNode(w_ho);
-			VariableNode bias_h = new VariableNode(b_h);
-			VariableNode bias_o = new VariableNode(b_o);
-
-			trainingVariables = new VariableNode[5];
-			trainingVariables[0] = weight_ih;
-			trainingVariables[1] = weight_hh;
-			trainingVariables[2] = weight_ho;
-			trainingVariables[3] = bias_h;
-			trainingVariables[4] = bias_o;
-
-			// Build the whole graph.
-			trainingGraph = new Graph();
-			lossNode = null;
-			trainingInputNodes = new InputNode[numTrainingStepsUnrolled];
-			trainingTargetNodes = new InputNode[numTrainingStepsUnrolled];
-			trainingStartHiddenNode = new InputNode(1, hiddenSize);
-			Node previousHidden = trainingStartHiddenNode;
-			for (int i = 0; i < numTrainingStepsUnrolled; i++) {
-				Node[] step = makeRNN(trainingGraph, weight_ih, weight_hh, weight_ho, bias_h, bias_o, previousHidden, true);
-				trainingInputNodes[i] = step[0];
-				previousHidden = step[1];
-				trainingTargetNodes[i] = step[3];
-				Node loss = step[4];
-
-				if (lossNode == null) {
-					lossNode = loss;
-				} else {
-					lossNode = new AddNode(lossNode, loss);
-				}
-			}
-			lossNode = new RowSumNode(lossNode);
-			trainingGraph.addNode(lossNode);
-			trainingOutputHiddenNode = previousHidden;
-		}
-
-		Matrix[] results = new Matrix[trainingVariables.length];
-		Matrix hiddenState = new Matrix(1, hiddenSize);
-
-		int numTrainingChunks = inputData.length/numTrainingStepsUnrolled;
-		for(int chunkIndex=0; chunkIndex < numTrainingChunks; chunkIndex++) {
-			for (int i=0; i < numTrainingStepsUnrolled; i++) {
-				inputMap.put(trainingStartHiddenNode, hiddenState);
-				inputMap.put(trainingInputNodes[i], new Matrix(1, inputSize, inputData[i+(chunkIndex*numTrainingStepsUnrolled)]));
-				inputMap.put(trainingTargetNodes[i], new Matrix(1, inputSize, outputData[i+(chunkIndex*numTrainingStepsUnrolled)]));
-			}
-
-			// Calculate the gradient for each of those values.
-			// The funkiness with using ID is because we want to allocate at least that many spaces for gradients.
-			Matrix[] fwd = trainingGraph.forward(inputMap);
-			Matrix[] grads = trainingGraph.getGradient(inputMap, fwd, lossNode);
-
-			if (reportFunction != null) {
-				reportFunction.accept(fwd[lossNode.id].get(0, 0));
-			}
-
-			// Smooth/average the gradients.
-			/*
-			for(int i=0; i < totalGradient.length; i++) {
-				totalGradient[i].elementOp_i(v -> v/(double)inputData.length);
-			}
-			*/
-			for(int i = 0; i < trainingVariables.length; i++) {
-				if(results[i] == null) {
-					results[i] = grads[trainingVariables[i].id];
-				} else {
-					results[i].elementOp_i(grads[trainingVariables[i].id], (a, b) -> a + b);
-				}
-			}
-			hiddenState = fwd[trainingOutputHiddenNode.id];
-		}
-		return results;
 	}
 
 	public double[] encodeCharacterToProbabilityDistribution(String s) {
@@ -240,60 +63,6 @@ public class RNNTest {
 		return "\n";
 	}
 
-	public String sampleOutput(int seed) {
-		// Build graph.
-		if(runGraph == null) {
-			runGraph = new Graph();
-
-			VariableNode weight_ih = new VariableNode(w_ih);
-			VariableNode weight_hh = new VariableNode(w_hh);
-			VariableNode weight_ho = new VariableNode(w_ho);
-			VariableNode bias_h = new VariableNode(b_h);
-			VariableNode bias_o = new VariableNode(b_o);
-
-			sampleHiddenInputNode = new InputNode(1, hiddenSize);
-
-			Node[] step = makeRNN(runGraph, weight_ih, weight_hh, weight_ho, bias_h, bias_o, sampleHiddenInputNode, false);
-
-			sampleInputNode = step[0];
-			sampleHiddenOutputNode = step[1];
-			sampleOutputNode = step[2];
-		}
-
-		// Step n steps.
-		String s = "";
-		Matrix lastOut = new Matrix(1, inputSize);
-		Matrix lastHidden = new Matrix(1, hiddenSize);
-		lastOut.data[seed] = 1.0;
-		for(int i=0; i < 100; i++) {
-			// Set our input states.
-			HashMap<Node, Matrix> inputFeed = new HashMap<>();
-			inputFeed.put(sampleInputNode, lastOut);
-			inputFeed.put(sampleHiddenInputNode, lastHidden);
-
-			// Push through.
-			Matrix[] states = runGraph.forward(inputFeed);
-
-			// Convert output to a character and use it as input.
-			String nextChar = selectCharacterFromProbabilityDistribution(states[sampleOutputNode.id].data);
-			if(nextChar == "\n") {
-				nextChar = "[\\n]";
-			}
-			s += nextChar;
-			lastOut = states[sampleOutputNode.id];
-			lastHidden = states[sampleHiddenOutputNode.id];
-		}
-
-		// Also, save the graph since we have it.
-		try(BufferedWriter fout = new BufferedWriter(new FileWriter("languageModal.dat"))) {
-			fout.write(runGraph.serializeToString());
-		} catch (IOException ioe) {
-
-		}
-
-		return s;
-	}
-
 	public TrainingPair sentenceToTrainingPair(String s) {
 		TrainingPair t = new TrainingPair();
 		t.x = new double[s.length()][];
@@ -308,72 +77,47 @@ public class RNNTest {
 	}
 
 	@Test
-	public void train() {
-		// Build the model's key pieces.
-
-		// Grab all the sentences.
+	public void run() {
+		// Load data.
 		ArrayList <String> sentences = new ArrayList<>();
 		try {
 			//Scanner scanner = new Scanner(new File("SICK_train.txt"));
-			Scanner scanner = new Scanner(new File("sentences.txt"));
+			//Scanner scanner = new Scanner(new File("sentences.txt"));
+			Scanner scanner = new Scanner(new File("tiny-shakespeare.txt"));
 			while(scanner.hasNext()) {
 				/*
 				String line = scanner.nextLine();
 				String[] chunks = line.split("\t");
 				sentences.add(chunks[1]);
 				*/
-				sentences.add(scanner.nextLine());
+				String sentence = scanner.nextLine();
+				if(sentence.length() > 5) {
+					sentences.add(scanner.nextLine());
+				}
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
+		// Build the model.
+		LSTM lstm = new LSTM(inputSize, hiddenSize);
+
 		// For each batch,
 		final int REPORT_INTERVAL = 100;
-		double learningRate = 0.5;
-		Matrix mem_W_ih = new Matrix(w_ih.rows, w_ih.columns);
-		Matrix mem_W_hh = new Matrix(w_hh.rows, w_hh.columns);
-		Matrix mem_W_ho = new Matrix(w_ho.rows, w_ho.columns);
-		Matrix mem_B_h = new Matrix(b_h.rows, b_h.columns);
-		Matrix mem_B_o = new Matrix(b_o.rows, b_o.columns);
+		double learningRate = 0.05;
 
 		int iteration = 0;
 		while(learningRate > 0 && iteration < 10000000) {
 			iteration += 1;
 
 			String sent = sentences.get(random.nextInt(sentences.size()));
-			//String sent = "Let's see how long it takes to fit this."; // About 2k iters.
+			//String sent = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; // About 2k iters.
 			sent = sent.replace('~', ' ');
 			sent = echoStringWhileLessThanMinLength(sent, numTrainingStepsUnrolled);
 			TrainingPair p = sentenceToTrainingPair(sent);
 
 			// Apply the training and, every N iterations, get a loss report.
-			DoubleConsumer report = null;
-			if(iteration % REPORT_INTERVAL == 0) {
-				report = i -> System.out.println("Loss : " + i);
-			}
-			Matrix[] grads = unrollAndTrain(p.x, p.y, report);
 
-			// Cap the gradients.
-			for(Matrix m : grads) {
-				capGradientAtL1Norm(m, 5.0f);
-			}
-
-			// Apply gradients with training loss.
-			adaGradUpdate(mem_W_ih, grads[0], w_ih, learningRate);
-			adaGradUpdate(mem_W_hh, grads[1], w_hh, learningRate);
-			adaGradUpdate(mem_W_ho, grads[2], w_ho, learningRate);
-			adaGradUpdate(mem_B_h, grads[3], b_h, learningRate);
-			adaGradUpdate(mem_B_o, grads[4], b_o, learningRate);
-
-			// For some reason, reassignment appears to be faster than in-place operations here.
-			//b_o = b_o.elementOp(grads[4], (w, dw) -> w - dw*learningRate);
-
-			if(iteration % REPORT_INTERVAL == 0) {
-				learningRate -= Math.pow(learningRate, 4);
-				//learningRate = Math.pow(1.0/Math.sqrt(iteration), 0.1);
-				System.out.println("Iter: " + iteration + "\tLR: " + learningRate +"\tSample: " + sampleOutput(random.nextInt(127-32)+32));
-			}
 		}
 	}
 
